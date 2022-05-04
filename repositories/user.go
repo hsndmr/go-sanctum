7d81c6c890f22eaf1eb81b3cb06b7a12f547bcfb
Hasan Demir <hasan.demir@tarfin.com>
2022-05-04 17:30:26 +0300

    Fix style

--- repositories/user.go
+++ repositories/user.go
@@ -28,27 +28,27 @@ type UserRepository struct {
 // Create creates a user and returns the user.
 func (r *UserRepository) Create(dto *CreateUserDto) (*ent.User, error) {
 	return r.db.Client().User.
-						Create().
-						SetEmail(dto.Email).
-						SetPassword(r.Crypto.HashPassword(dto.Password)).
-						SetName(dto.Name).
-						Save(r.ctx)
+		Create().
+		SetEmail(dto.Email).
+		SetPassword(r.Crypto.HashPassword(dto.Password)).
+		SetName(dto.Name).
+		Save(r.ctx)
 }
 
 // FindByEmail finds a user by email and returns the user.
 func (r *UserRepository) FindByEmail(email string) (*ent.User, error) {
 	return r.db.Client().User.
-						Query().
-						Where(user.Email(email)).
-						Only(r.ctx)
+		Query().
+		Where(user.Email(email)).
+		Only(r.ctx)
 }
 
 // FindById finds a user by id and returns the user.
 func (r *UserRepository) FindByID(id int) (*ent.User, error) {
 	return r.db.Client().User.
-						Query().
-						Where(user.ID(id)).
-						Only(r.ctx)
+		Query().
+		Where(user.ID(id)).
+		Only(r.ctx)
 }
 
 func (r *UserRepository) ToInterfaceForJson(u *ent.User) interface{} {
